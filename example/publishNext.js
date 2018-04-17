3714c6cbc063bce9bc7c95c7075c50e11c9e89ab
Jeremy Yang <kaiwei19@gmail.com>
2018-04-17 10:39:13 +0800

    Update publishNext with timer

--- example/publishNext.js
+++ example/publishNext.js
@@ -6,16 +6,23 @@ const iota = new IOTA({ provider: `http://140.112.28.112/iota` })
 let mamState = Mam.init(iota)
 
 const publishNext = async packet => {
+    console.time("updateChannelRoot")
     mamState = await Mam.updateChannelRoot(mamState)
+    console.timeEnd("updateChannelRoot")
+
     let packetTrytes = iota.utils.toTrytes(JSON.stringify(packet))
     let message = Mam.create(mamState, packetTrytes)
 
+    console.time("attach")
     await Mam.attach(message.payload, message.address)
+    console.timeEnd("attach")
 
     let firstRoot = Mam.getFirstRoot(mamState)
+    console.time("fetch")
     var resp = await Mam.fetch(firstRoot, 'public')
-    for (var msg in resp.messages) {
-        console.log(iota.utils.fromTrytes(msg))
+    console.timeEnd("fetch")
+    for (var i = 0; i < resp.messages.length; i++) {
+        console.log(iota.utils.fromTrytes(resp.messages[i]))
     }
 }
 
