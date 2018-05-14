b6bd99e81db207a890f594b59afd451fc45affe9
Jeremy Yang <kaiwei19@gmail.com>
2018-05-14 19:20:45 +0800

    Merge case code updated

--- example/mergeCase.js
+++ example/mergeCase.js
@@ -1,38 +1,40 @@
 const Mam = require('../lib/mam.client.js')
 const IOTA = require('iota.lib.js')
-const iota = new IOTA({ provider: `http://140.112.28.112/iota` })
+const iota = new IOTA({ provider: `http://192.168.0.110:14265` })
+
+var seedJSON = require('./Seeds.json')
 
 const channelSeedIndex = [
-    [Mam.keyGen(81), 1],
-    [Mam.keyGen(81), 1],
-    [Mam.keyGen(81), 1],
-    [Mam.keyGen(81), 1],
+    [seedJSON.seed[0], 0],
+    [seedJSON.seed[1], 1],
+    [seedJSON.seed[2], 1]
 ]
 
 const seed = Mam.keyGen(81)
 
-let mamState = Mam.init(iota, seed)
+let mamState = Mam.init(iota, seed, 1)
 
 const mergeChannel = async channelSeeds => {
     let mergedChannelRoots = []
     for (let [channelSeed, index] of channelSeeds) {
-        let subMamState = Mam.init(iota, channelSeed)
+        let subMamState = Mam.init(iota, channelSeed, 1)
         mergedChannelRoots.push(Mam.getFirstRoot(subMamState))
         subMamState.channel.start = index
         let payload = {
             message: 'channel merged',
         }
         let message = Mam.createWithJson(subMamState, payload, Mam.getFirstRoot(mamState))
-        await Mam.attach(message.payload, message.address, 4, 11)
+	await Mam.attach(message.payload, message.address, 4, 11)
     }
-
-
-    let payload = {
-        state: "OK",
-        timestamp: Date.now(),
-        mergedChannelRoots: mergedchannelRoots,
+    
+    let msgJson = {
+	productID: 666,
+	productName: "merged_product",
+	stage: 0,
+	timestamp: Date.now(),
+	componentRoots:mergedChannelRoots
     }
-    let message = Mam.createWithJson(mamState, payload)
+    let message = Mam.createWithJson(mamState, msgJson)
     console.log('msg address: ', message.address)
     Mam.attach(message.payload, message.address, 4, 11)
 }
