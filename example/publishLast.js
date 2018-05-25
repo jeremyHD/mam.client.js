8a27a828be7d7998a796d50dd39ef4df43f0425a
Jeremy Yang <kaiwei_b88019@msn.com>
2018-05-25 12:28:52 +0800

    update PublishLast

--- example/publishLast.js
+++ example/publishLast.js
@@ -1,6 +1,6 @@
 const Mam = require('../lib/mam.client.js')
 const IOTA = require('iota.lib.js')
-const iota = new IOTA({ provider: `http://localhost:14265` })
+const iota = new IOTA({ provider: `http://192.168.0.110:14265` })
 
 const nextRoot = '999999999999999999999999999999999999999999999999999999999999999999999999999999999'
 
@@ -8,7 +8,7 @@ var args = process.argv.slice(2)
 var seedJSON = require('./Seeds.json')
 
 // Initialise MAM State
-let mamState = Mam.init(iota, seedJSON.seed[parseInt(args[0])], 1)
+let mamState = Mam.init(iota, seedJSON.seed[parseInt(args[0])])
 
 
 const publishLast = async (index) => {
