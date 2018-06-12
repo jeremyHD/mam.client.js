c4e0abac0c3baaf084df80daed7f0676d7dff92d
Jeremy Yang <kaiwei19@gmail.com>
2018-06-12 21:10:37 +0800

    update fetch json example

--- example/fetchJson.js
+++ example/fetchJson.js
@@ -1,18 +1,19 @@
 const Mam = require('../lib/mam.client.js')
 const IOTA = require('iota.lib.js')
-const iota = new IOTA({ provider: `http://localhost:14265` })
+const iota = new IOTA({ provider: 'http://192.168.0.110:14265' })
 
 var args = process.argv.slice(2)
 
 // Initialise MAM State
-let mamState = Mam.init(iota)
+let mamState = Mam.init(iota, Mam.keyGen(81), 1)
 
 let root = args[0]
 
 const execute = async address => {
     const resp = await Mam.fetch(address, 'public')
     for (var i = 0; i < resp.messages.length; i++) {
-        console.log(iota.utils.fromTrytes(resp.messages[i]))
+        let message = JSON.parse(iota.utils.fromTrytes(resp.messages[i]))
+        console.log(message)
     }
     console.log('next root: ', resp.nextRoot)
 }
--- example/fetchJsonWithSeeds.js
+++ example/fetchJsonWithSeeds.js
@@ -1,12 +1,14 @@
 const Mam = require('../lib/mam.client.js')
 const IOTA = require('iota.lib.js')
-const iota = new IOTA({ provider: `http://localhost:14265` })
+const iota = new IOTA({ provider: 'http://192.168.0.110:14265' })
 
 var args = process.argv.slice(2)
 var seedJSON = require('./Seeds.json')
 
 // Initialise MAM State
-let mamState = Mam.init(iota, seedJSON.seed[parseInt(args[0])])
+let mamState = Mam.init(iota, seedJSON.seed[parseInt(args[0])], 1)
+mamState.channel.count = 2
+mamState.channel.next_count = 2
 
 let roots = [Mam.getFirstRoot(mamState)]
 
@@ -15,7 +17,8 @@ const execute = async addresses => {
         console.log('address: ', address)
         const resp = await Mam.fetch(address, 'public')
         for (var i = 0; i < resp.messages.length; i++) {
-            console.log(iota.utils.fromTrytes(resp.messages[i]))
+            let messageJson = JSON.parse(iota.utils.fromTrytes(resp.messages[i]))
+            console.log(messageJson)
         }
         console.log('next root: ', resp.nextRoot)
     }
